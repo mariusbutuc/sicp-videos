8db51d01b814c64471e1fb7ef18adc07c781a76c
Marius Butuc <marius.butuc@shopify.com>
2015-02-27 18:08:10 -0500

    Keep Structs light. Use Classes otherwise.

--- get_videos.rb
+++ get_videos.rb
@@ -22,47 +22,8 @@ SICP_VIDEOS = {
   '10a' => 'Compilation',
   '10b' => 'Storage Allocation and Garbage Collection'
 }.freeze
+require_relative 'sicp_video'
 
-SicpVideo = Struct.new(:id, :title) do
-  require 'open-uri'
-
-  attr_accessor :id, :title
-
-  def initialize(id, title)
-    @id = id
-    @title = title
-  end
-
-  def slug
-    @slug ||= @title.gsub(/\W+/, '-').downcase
-  end
-
-  def url
-    @url ||= "http://www.archive.org/download/MIT_Structure_of_Computer_Programs_1986/lec#{@id}_512kb.mp4"
-  end
-
-  def srt
-    @srt ||= "http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/video-lectures/#{@id}-#{slug}/lec#{@id}_512kb.srt"
-  end
-
-  def download_video
-    download("out/#{@id}-#{slug}.mp4", url)
-  end
-
-  def download_subtitle
-    download("out/#{@id}-#{slug}.srt", srt)
-  end
-
-  private
-
-  def download(file_out, url)
-    File.open(file_out, "wb") do |saved_file|
-      open(url, "rb") do |read_file|
-        saved_file.write(read_file.read)
-      end
-    end
-  end
-end
 
 SICP_VIDEOS.each_with_index do |(part, title), index|
   video = SicpVideo.new(part, title)
